ff5299a9802e1f628e5cdf7aa1a784a15e770c59
shubhamAW <waykarshubham2022@gmail.com>
2022-11-28 10:00:21 +0530

    improve small changes

--- components/Bta.js
+++ components/Bta.js
@@ -9,7 +9,7 @@ const Bta = ({title , desc, btn ,image }) => {
     <div  className='flex flex-col md:flex-row md:justify-between my-14 mt-36 '>
         <Image src={image} className="md:hidden flex-grow"  alt="image"/>
         {/* left */}
-       <div className='space-y-4 w-1/2'>
+       <div className='space-y-4  md:w-1/2'>
         <h1 className='text-4xl font-bold '>{title}</h1>
         <p className='font-semibold text-gray-500 text-[18px]'>{desc}</p>
         <button className={`border bg-red-400  px-4 py-2 rounded-md font-bold text-white hover:shadow-xl`}>{btn}</button>
